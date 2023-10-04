4ffcbb347c041d63f6907052961bebc75dae18bf
김성호 <nareun130@gmail.com>
2023-10-05 02:39:30 +0900

    [FEAT] : 람다식으로 더 간편하게 처리

--- with-java/src/ListPrint/FP02Functional.java
+++ with-java/src/ListPrint/FP02Functional.java
@@ -10,11 +10,15 @@ public class FP02Functional {
         printEvenNumbers(numbers);
     }
 
-    private static boolean isEven(int number) {
-        return number % 2 == 0;
-    }
+    // private static boolean isEven(int number) {
+    //     return number % 2 == 0;
+    // }
 
     private static void printEvenNumbers(List<Integer> numbers) {
-        numbers.stream().filter(FP02Functional::isEven).forEach(System.out::println);
+        // numbers.stream().filter(FP02Functional::isEven).forEach(System.out::println);
+        // ~> 람다식으로 더 간편하게
+        numbers.stream().filter(number -> number % 2 == 0).forEach(System.out::println);
+        //! 이 처럼 함수형 프로그래밍에서는 수행할 작업에 초점을 마춘다.
+
     }
 }
